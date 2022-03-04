0f5c3f9d64f1e43e90b617c408a850c165105303
abistong <abistong@gmail.com>
2022-03-03 21:40:47 -0500

    Ejercicios2 de examen practico

---
+++ ejercicios2.js
@@ -0,0 +1,57 @@
+/*
+var a = Math.floor (Math.random()*100);
+alert(a);
+
+if(a % 2 != 0){
+    console.log("El numero es par");
+    }
+    else{
+        console.log("El numero es impar");
+    }
+*/
+
+
+/*
+var i;
+var j = 2;
+var rep = 0;
+
+for (i = 1; i <= j; i++){
+    document.write(i);
+
+    if (i % 3 == 0){
+    document.write("(multiplo de 3)");
+    }
+}
+*/
+
+
+
+/*
+let user_in = prompt("escribe una palabra: ");
+let myChar = 'a';
+countLetter = 0;
+for(let i = 0; i < user_in.length; i++){
+  if(user_in.charAt(i) === myChar){
+    countLetter++;
+  }
+}
+
+console.log(`Se encontro ${countLetter}  letras '${myChar}' en tu palabra!!!`)
+*/
+
+
+
+/*
+const array1 = [20,50,100];
+console.log('array1:', array1);
+
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+*/
+
+/*
+let cadena = 'flor de la mañana';
+console.log(cadena.toUpperCase());
+ */
